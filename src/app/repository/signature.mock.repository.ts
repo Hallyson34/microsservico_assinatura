d6df9c1f594f5d0086dcc618d78f962d924b0403
trunabeles <bruna.amarilha@ufms.br>
2023-11-14 15:43:18 -0400

    Added async to repository

--- src/app/repository/signature.mock.repository.ts
+++ src/app/repository/signature.mock.repository.ts
@@ -5,24 +5,24 @@ import { LocalDatabase } from '../database/local/localDatabase';
 export class SignatureMockRepository implements SignatureRepositoryInterface {
   constructor(private localDatabase = LocalDatabase.createConnection()) {}
 
-  create(plan_id: number,user_id: number,period: number,due_day: number,): Promise<SignatureEntity> {
+  async create(plan_id: number,user_id: number,period: number,due_day: number,): Promise<SignatureEntity> {
     const id = this.localDatabase.signature.getNextId();
     const entity: SignatureEntity = SignatureEntity.create(id, plan_id, user_id, period, due_day);
     return this.localDatabase.signature.create(entity);
   }
-  updateDueDay(signatureEntity: SignatureEntity): Promise<SignatureEntity> {
+  async updateDueDay(signatureEntity: SignatureEntity): Promise<SignatureEntity> {
     return this.localDatabase.signature.update(signatureEntity);
   }
-  deactivate(signatureEntity: SignatureEntity): Promise<SignatureEntity> {
+  async deactivate(signatureEntity: SignatureEntity): Promise<SignatureEntity> {
     return this.localDatabase.signature.update(signatureEntity);
   }
-  findById(id: number): Promise<SignatureEntity> {
+  async findById(id: number): Promise<SignatureEntity> {
     return this.localDatabase.signature.findById(id);
   }
-  findAllByUserId(userId: number): Promise<SignatureEntity[]> {
+  async findAllByUserId(userId: number): Promise<SignatureEntity[]> {
     return this.localDatabase.signature.findAllByUserId(userId);
   }
-  findActiveByUserId(userId: number): Promise<SignatureEntity> {
+  async findActiveByUserId(userId: number): Promise<SignatureEntity> {
     return this.localDatabase.signature.findActiveByUserId(userId);
   }
 }
