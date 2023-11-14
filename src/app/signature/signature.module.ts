67c078b857f14288bc2447e1aa2ff06dd70c39c1
Hallyson <hallysonnobres@gmail.com>
2023-11-14 18:48:42 -0400

    added module

--- src/app/signature/signature.module.ts
+++ src/app/signature/signature.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SignatureController } from './signature.controller';
+import { SignatureService } from './signature.service';
+import { SignatureMockRepository } from '../repository/signature.mock.repository';
 
 @Module({
   controllers: [SignatureController],
+  providers: [
+    SignatureService,
+    {
+      provide: 'SignatureMockRepository',
+      useClass: SignatureMockRepository,
+    },
+  ],
 })
 export class SignatureModule {}
